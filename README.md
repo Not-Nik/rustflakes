5ed1a52857cefa81a9fe35c9ce316a7ce5616bd3
Not-Nik <nik.wipper@gmx.de>
2020-08-09 16:35:06 +0200

    Add snowflake decoding

--- README.md
+++ README.md
@@ -8,7 +8,16 @@ use rustflakes::SnowflakeWorker;
 fn main() {
     let worker_id = 0;
     let process_id = 0;
-    let worker = SnowflakeWorker::new(worker_id, process_id);
+    let mut worker = SnowflakeWorker::new(worker_id, process_id);
     println!("{}", &worker.make());
 }
 ```
+## Structure
+Snowflakes are structured as defined in the [Discord API documentation](https://discord.com/developers/docs/reference#snowflakes-snowflake-id-format-structure-left-to-right).
+
+Field               | Bits     | Number of Bits | Description
+--------------------|----------|----------------|------------
+Timestamp           | 63 to 22 | 42 bits        | Milliseconds since Discord Epoch, the first second of 2015 or 1420070400000.
+Internal worker ID  | 21 to 17 | 5 bits         |
+Internal process ID | 16 to 12 | 5 bits         |
+Increment           | 11 to 0  | 12 bits        | For every ID that is generated on that process, this number is incremented
--- src/lib.rs
+++ src/lib.rs
@@ -2,47 +2,81 @@ use std::time::{SystemTime, UNIX_EPOCH};
 
 #[cfg(test)]
 mod tests {
-    use crate::{SnowflakeWorker};
+    use crate::{SnowflakeWorker, Snowflake};
 
     #[test]
     fn test() {
-        let mut worker = SnowflakeWorker::new(0, 0);
+        let mut worker = SnowflakeWorker::new(1, 2);
         // These might fail on an unreasonably slow computer
         assert_eq!(SnowflakeWorker::get_timestamp(), worker.make() >> 22);
         worker.increment = 42;
-        assert_eq!(42, /*Clear the left-most 52 bits*/ worker.make() << 52 >> 52);
+        let snowflake = Snowflake::from(worker.make());
+        assert_eq!(snowflake.increment, 42);
+        assert_eq!(snowflake.worker_id, 1);
+        assert_eq!(snowflake.process_id, 2);
     }
 
     #[test]
     fn readme() {
         let worker_id = 0;
         let process_id = 0;
-        let worker = SnowflakeWorker::new(worker_id, process_id);
+        let mut worker = SnowflakeWorker::new(worker_id, process_id);
         println!("{}", &worker.make());
+    }
+
+    fn discord_doc() {
+        let snowflake = Snowflake::from(175928847299117063);
+        assert_eq!(snowflake.timestamp, 1462015105796);
+        assert_eq!(snowflake.worker_id, 1);
+        assert_eq!(snowflake.process_id, 0);
+        assert_eq!(snowflake.increment, 7);
     }
 }
 
 pub struct SnowflakeWorker
 {
-    worker_id: i64,
-    process_id: i64,
-    increment: i64
+    worker_id: u64,
+    process_id: u64,
+    increment: u64
 }
 
 impl SnowflakeWorker
 {
-    pub fn new(worker_id: i64, process_id: i64) -> SnowflakeWorker
+    pub fn new(worker_id: u64, process_id: u64) -> SnowflakeWorker
     {
         SnowflakeWorker { worker_id, process_id, increment: 0 }
     }
 
-    fn get_timestamp() -> i64 {
+    fn get_timestamp() -> u64 {
         SystemTime::now().duration_since(UNIX_EPOCH)
             .expect("")
-            .as_millis() as i64 - 1420070400000
+            .as_millis() as u64 - 1420070400000
     }
 
-    pub fn make(&self) -> i64 {
-        SnowflakeWorker::get_timestamp() << 22 | self.worker_id << 17 | self.process_id << 12 | self.increment
+    pub fn make(&mut self) -> u64 {
+        self.increment += 1;
+        SnowflakeWorker::get_timestamp() << 22 | self.worker_id << 17 | self.process_id << 12 | (self.increment - 1)
+    }
+}
+
+pub struct Snowflake {
+    /// Unix timestamp
+    pub timestamp: u64,
+    /// Worker that created this Snowflake, internal
+    pub worker_id: u64,
+    /// Process that created this Snowflake, internal
+    pub process_id: u64,
+    /// Increment of this Snowflake, internal
+    pub increment: u64
+}
+
+impl Snowflake {
+    pub fn from(snowflake: u64) -> Snowflake {
+        Snowflake {
+            timestamp: (snowflake >> 22) + 1420070400000,
+            worker_id: (snowflake & 0x3E0000) >> 17,
+            process_id: (snowflake & 0x1F000) >> 12,
+            increment: snowflake & 0xFFF
+        }
     }
 }
